a99c45ea1021ebf7eaaca13921708196e3159b47
Aleksander Matusiak <am347171@students.mimuw.edu.pl>
2015-03-23 15:33:20 +0100

    Added product validations and average_rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,20 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  #  Validations
+  validates_presence_of :title
+  validates_presence_of :price
+  validates_presence_of :description
+  validates_format_of :price, :with => /\A\d+(?:\.\d{0,2})?\z/
+
+  # Computes average rating
+  def average_rating
+    sum = 0.0
+    self.reviews.each do |r|
+      sum += r.rating
+    end
+    return sum / self.reviews.count
+  end
+
 end
