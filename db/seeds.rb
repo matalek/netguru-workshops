6ca133aa15aa82125d2c8a9ae2ec4798c864815a
Aleksander Matusiak <am347171@students.mimuw.edu.pl>
2015-03-28 16:12:13 +0100

    Added seeds for admin and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: 'admin@example.com', firstname: 'Great', lastname: 'Admin',
+             password: 'adminadmin', password_confirmation: 'adminadmin', admin: true)
+
+User.create(email: 'jan@kowalski.pl', firstname: 'Jan', lastname: 'Kowalski',
+            password: 'tajnehaslo', password_confirmation: 'tajnehaslo')
+
+User.create(email: 'piotrus@poczta.pl', firstname: 'Piotr', lastname: 'Bajeczny',
+            password: 'ppiioottrr', password_confirmation: 'ppiioottrr')
+
+User.create(email: 'akoza@skrzynka.waw.pl', firstname: 'Andrzej', lastname: 'Kozak',
+            password: 'kozacko123', password_confirmation: 'kozacko123')
+
+User.create(email: 'usmieszek123@abc.com', firstname: 'Anna', lastname: 'Krzywicka',
+            password: 'anusia88', password_confirmation: 'anusia88')
+
+User.create(email: 'maria@borsuki.pl', firstname: 'Mari', lastname: 'Borsuk',
+            password: 'fajnehaslo', password_confirmation: 'fajnehaslo')
