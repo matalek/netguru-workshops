09528b12c86035e58af6b63c1747cc899f9f1499
Aleksander Matusiak <am347171@students.mimuw.edu.pl>
2015-03-30 16:58:15 +0200

    Updated user's reviews display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,9 @@ class UsersController < ApplicationController
       @admin = 'No'
     end
     @reviews = @user.reviews.order(created_at: :desc).first(5)
+    if !@reviews.any?
+      @none_reviews = true
+    end
     @display_product = true
   end
 end
