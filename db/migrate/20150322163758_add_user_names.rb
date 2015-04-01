805fa86faedb623eb6a7f84c316648f55698c601
Aleksander Matusiak <am347171@students.mimuw.edu.pl>
2015-04-01 23:42:56 +0200

    Corrected typo in migrations

--- db/migrate/20150322163758_add_user_names.rb
+++ db/migrate/20150322163758_add_user_names.rb
@@ -1,6 +1,6 @@
 class AddUserNames < ActiveRecord::Migration
   def change
     add_column :users, :firstname, :string
-    add_column :users, :lastname, :stringrak
+    add_column :users, :lastname, :string
   end
 end
